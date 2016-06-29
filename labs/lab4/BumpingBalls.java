4fc9d4207e9c6587f240c59f244e9722be5e4d43
Doug Shook <dsshook@gmail.com>
2016-06-29 16:53:42 -0500

    Adding materials for studio 4 and lab 4

---
+++ labs/lab4/BumpingBalls.java
@@ -0,0 +1,9 @@
+package lab4;
+
+public class BumpingBalls {
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ studios/studio4/Flag.java
@@ -0,0 +1,14 @@
+package studio4;
+
+import sedgewick.StdDraw;
+
+public class Flag {
+
+	
+	public static void main(String[] args) {
+		//
+		//  Add code for your studio below here.
+		//
+	}
+
+}
