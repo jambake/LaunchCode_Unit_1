a2c2311d21b1ac7316c29e7143edddd51a7ccb83
Doug Shook <dsshook@gmail.com>
2016-07-07 15:30:00 -0500

    Adding stuff for module 5

---
+++ exercises/exercises5/Recipe.java
@@ -0,0 +1,20 @@
+package exercises5;
+
+public class Recipe {
+
+	public static void main(String[] args) {
+
+		//
+		// No java code needed for this exercise
+		// Below, use comments or actual Java code
+		//  to type in the abstractions
+		//  you found in the recipe story
+		// 
+
+		//
+		// Use those abstractions in comments or code to 
+		//   express the recipe
+		//
+	}
+
+}
---
+++ studios/studio5/Methods.java
@@ -0,0 +1,26 @@
+package studio5;
+
+public class Methods {
+	
+	/**
+	 * Implemented correctly
+	 * @param x one thing to add
+	 * @param y the other thing to add
+	 * @return the sum
+	 */
+	public static int sum(int x, int y) {
+		return x+y;
+	}
+	
+	/**
+	 * 
+	 * @param x one factor
+	 * @param y another factor
+	 * @return the product of the factors
+	 */
+	public static int mpy(int x, int y) {
+		return 0;  // FIXME
+	}
+
+
+}
